5d0ae10cd66fe5e65036fd15dc70666077daab99
NayMac <itzel-silva@hotmail.com>
2018-10-29 21:15:19 -0600

    Works

--- apps/estado/urls.py
+++ apps/estado/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url, include
-from apps.estado.views import index_estado
+from django.urls import path
+from apps.estado.views import *
+app_name = 'estado'
 
 urlpatterns = [
-    url(r'^$', index_estado),
+   # url(r'^$', index_estado),
+    path('nuevo/', Agregar.as_view(), name='crear'),
+    path('editar/<pk>/', Editar.as_view(), name='editar'),
+    path('eliminar/<pk>/', Eliminar.as_view(), name='eliminar'),
+    path('listar/', Listar.as_view(), name='listar'),
 ]
--- apps/estado/views.py
+++ apps/estado/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apps.estado.forms import EstadoForm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from django.urls import reverse_lazy
+from apps.estado.models import Estado
 
 def index_estado(request):
 	return HttpResponse("Segunda pagina de estado")
+
+class Agregar(CreateView):
+	model = Estado
+	template_name = 'estado/estado_form.html'
+	form_class = EstadoForm
+	success_url = reverse_lazy('estado:listar')
+
+
+class Editar(UpdateView):
+	model = Estado
+	form_class = EstadoForm
+	template_name = 'estado/estado_form.html'
+	success_url = reverse_lazy('estado:listar')
+
+
+class Eliminar(DeleteView):
+	model = Estado
+	template_name = 'estado/estado_delete.html'
+	success_url = reverse_lazy('estado:listar')
+
+
+class Listar(ListView):
+	model = Estado
+	template_name = 'estado/estado_list.html'
